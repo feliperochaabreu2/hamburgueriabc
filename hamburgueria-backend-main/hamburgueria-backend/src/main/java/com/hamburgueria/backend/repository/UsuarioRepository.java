e84f72199714770410042d59f5ab29c893596877
rafaelsantoscalcme <110471129+rafaelsantoscalcme@users.noreply.github.com>
2024-12-10 16:52:22 -0300

    removendo
    removendo comentarios

--- hamburgueria-backend-main/hamburgueria-backend/src/main/java/com/hamburgueria/backend/repository/UsuarioRepository.java
+++ hamburgueria-backend-main/hamburgueria-backend/src/main/java/com/hamburgueria/backend/repository/UsuarioRepository.java
@@ -1,23 +1,3 @@
-/*
-package com.hamburgueria.backend.repository;
-
-import java.util.Optional;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.hamburgueria.backend.model.Usuario;
-
-public interface UsuarioRepository extends MongoRepository<Usuario, String> {
-	
-	Usuario findByEmail(String email);
-	
-	Optional<Usuario> findFirstByEmail(String email);
-	
-	Optional<Usuario> findByEmailAndSenha(String email, String senha);
-
-}
-*/
-
 package com.hamburgueria.backend.repository;
 
 import java.util.Optional;
